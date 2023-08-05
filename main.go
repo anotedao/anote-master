474a93228eb69f6c3a59b88bac82b913a565de9f
Pragmatic <cryptopragmatic@protonmail.com>
2023-08-05 20:15:53 +0200

    changed reward fee

--- main.go
+++ main.go
@@ -22,12 +22,12 @@ func main() {
 
 	wc = initWaves()
 
-	val := "%s%s__3AVJ1R8481NeiPiaVn8HRXCU6Mp1GE6q8rV__3AWQfMMTNWYaPsqs4vmYb1SMhh3zy4RD5Vr"
-	dataTransaction("146.190.145.57", &val, nil, nil)
+	// val := "%s%s__3AVJ1R8481NeiPiaVn8HRXCU6Mp1GE6q8rV__3AWQfMMTNWYaPsqs4vmYb1SMhh3zy4RD5Vr"
+	// dataTransaction("146.190.145.57", &val, nil, nil)
 
-	lease("3AVJ1R8481NeiPiaVn8HRXCU6Mp1GE6q8rV")
+	// lease("3AVJ1R8481NeiPiaVn8HRXCU6Mp1GE6q8rV")
 
 	// leaseCancel("FF8tKf2dr3BQPCLw41StvRdU2dNhfeWMyJstNQdzASA1")
 
-	// startMonitor()
+	startMonitor()
 }
