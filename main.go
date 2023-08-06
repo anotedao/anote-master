715c9bea076ad38741c6bdeaf9ec472d8eb9af5c
Pragmatic <cryptopragmatic@protonmail.com>
2023-08-06 21:16:37 +0200

    removed extra imports

--- main.go
+++ main.go
@@ -2,25 +2,16 @@ package main
 
 import (
 	"log"
-
-	"github.com/wavesplatform/gowaves/pkg/client"
-	"gopkg.in/macaron.v1"
 )
 
 var conf *Config
-
-var nodeAddress string
-
-var m *macaron.Macaron
-
-var wc *client.Client
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	conf = initConfig()
 
-	wc = initWaves()
+	initWaves()
 
 	// val := "%s%s__3AVJ1R8481NeiPiaVn8HRXCU6Mp1GE6q8rV__3AWQfMMTNWYaPsqs4vmYb1SMhh3zy4RD5Vr"
 	// dataTransaction("146.190.145.57", &val, nil, nil)
