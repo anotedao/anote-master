cd6637f5fbbd2b2c1d92f225512f98981e17d877
Pragmatic <cryptopragmatic@protonmail.com>
2023-10-07 21:15:43 +0200

    new lease

--- main.go
+++ main.go
@@ -21,12 +21,12 @@ func main() {
 
 	// initWaves()
 
-	// val := "%s%s__3AUYBormLLPHoxwjkiFYcDqWvvdzKwNinaU__3AA1ziNL6nYGrJr9hDw8KY5JKD15HQQzLbz"
+	// val := "%s%s__3AVEnVqk9RpNj89Kbid1o68mXw4rR3SrsKw__3AEBPBgPx59KfgjyFvhS6mR6APSPfae38J6"
 	// dataTransaction("161.35.146.50", &val, nil, nil)
 
-	// lease("3AUYBormLLPHoxwjkiFYcDqWvvdzKwNinaU")
+	// lease("3AVEnVqk9RpNj89Kbid1o68mXw4rR3SrsKw")
 
 	// leaseCancel("FF8tKf2dr3BQPCLw41StvRdU2dNhfeWMyJstNQdzASA1")
 
-	startMonitor()
+	// startMonitor()
 }
