6c5ae7a806f4a41cbba5d7858c285c178bb0b215
Pragmatic <cryptopragmatic@protonmail.com>
2024-01-05 15:29:24 +0100

    leases

--- main.go
+++ main.go
@@ -21,9 +21,9 @@ func main() {
 	// val := "%s%s__3ACJZCJTDLSGu4n9i8UtnJgWaEDppXUodXs__3AShXVgRcRis82CwD7o9pz1Ac9vmRYMqELT"
 	// dataTransaction("188.25.72.123", &val, nil, nil)
 
-	// lease("3AYAhn6tKGssduMsCvnjEPa2MMSduRPW2gr")
-	// lease("3AArU5bWZ7xZBR2ygoB9nKiYQzvGFiq3PXF")
-	// lease("3AKZGfBftW9py18vUX8cuNT5jM3aDd4N1xM")
+	// lease("3AThTowRX96bQ22oXo2XPWWpp8C1kRoQMdQ")
+	// lease("3AUYBormLLPHoxwjkiFYcDqWvvdzKwNinaU")
+	// lease("3ATYNXJA6xe3mBrXSQU4rKyDZSkT8MKUowU")
 	// lease("3AC3LNRYZs7bj95D3bKruRFLdUmCJTmaidr")
 	// lease("3AJXGdrAgPYt218fNNwx15BW4EdyHeuo1hq")
 
