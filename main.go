d73a971b7837fd5e8089b122cc5b4f086eb94ca8
Pragmatic <cryptopragmatic@protonmail.com>
2024-01-18 20:12:07 +0100

    new lease

--- main.go
+++ main.go
@@ -18,11 +18,10 @@ func main() {
 
 	// initWaves()
 
-	// val := "%s%s__3ADvaNaDR7K3jrBumDjFkyTdxqkPAv8Jc3W__3AA5c4VhSjmE8C9eV1fezMdVE2yzujpBVV6"
-	// dataTransaction("188.25.138.154", &val, nil, nil)
+	// val := "%s%s__3ATm4u9ANgtUd4R4fXhuy9FCUjzARKCmhir__3AA5c4VhSjmE8C9eV1fezMdVE2yzujpBVV6"
+	// dataTransaction("188.25.138.155", &val, nil, nil)
 
-	// lease("3ADvaNaDR7K3jrBumDjFkyTdxqkPAv8Jc3W")
-	// leaseCancel("DuXMGMsrj5tLrTkCgX9Zr3W8eK1jCi19bY2Fs1P7McUL")
+	// lease("3ATm4u9ANgtUd4R4fXhuy9FCUjzARKCmhir")
 
 	startMonitor()
 }
