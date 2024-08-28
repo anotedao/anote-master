36cc9b698afa11897b67afb24ebcd9f929ced5eb
Pragmatic <cryptopragmatic@protonmail.com>
2024-08-29 00:58:25 +0200

    removing old nodes

--- main.go
+++ main.go
@@ -12,5 +12,13 @@ func main() {
 
 	conf = initConfig()
 
-	startMonitor()
+	// startMonitor()
+
+	// leaseCancel("FXZ36ZEdEVbkTVohnJjEeJbVTRjwCyhaC2kZEi838LLR")
+	// leaseCancel("B3dVWFM8mEyt9xJ9JuZKLrkAtaEGByZen8geYXzJbfqf")
+	// leaseCancel("8Ca2mX56VQayj5rAnffGR6J9NDHY19yLL1NKGerkKoBK")
+	// leaseCancel("BpxrYokyi4QW2k8qJkiGJV5t7fkpmCVVae2Un7pEVjyQ")
+	// leaseCancel("FNR5pyKQ98YgkgiZtd4iTdX3EMxaWF2wRXGaMq9jTeYL")
+
+	// dataTransaction("3APSKZokE7Hg7dnVDmVVWVQpqVEgiVW9XG9", nil, nil, nil)
 }
